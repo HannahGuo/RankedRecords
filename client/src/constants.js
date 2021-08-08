86bee6499a1bf97d6958191cd2846ae30ca68c29
HannahGuo <hguo2102@gmail.com>
2021-08-08 02:24:10 -0400

    troubleshooting env part 2

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,2 +1,2 @@
-export const CURRENT_URL = process.env.CURRENT_URL || "http://localhost:3000/";
-export const CURRENT_SERVER_URL = process.env.CURRENT_URL || "http://localhost:3001/";
+export const CURRENT_URL = process.env.REACT_APP_CURRENT_URL || "http://localhost:3000/";
+export const CURRENT_SERVER_URL = process.env.REACT_APP_CURRENT_URL || "http://localhost:3001/";
